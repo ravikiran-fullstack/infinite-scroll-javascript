e7fae2d27e87a7489642bab59a363e01e06e5611
ravikiran <ravikiransjce.jobs@gmail.com>
2022-03-03 06:34:41 +0530

    Added infinite scroll feature

---
+++ infinite-scroll.js
@@ -0,0 +1,32 @@
+console.log("infinite-scroll");
+window.onscroll = function (ev) {
+  if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+    // alert("you're at the bottom of the page");
+    createAndAppendImages();
+  }
+};
+
+function createAndAppendImages() {
+  const container = document.querySelector(".container");
+
+  const div = document.createElement("div");
+  div.setAttribute("id", "testing");
+  div.setAttribute("class", "imageDiv");
+  const image = document.createElement("img");
+
+  image.setAttribute(
+    "src",
+    `https://picsum.photos/id/${randomNumberGenerator()}/200/300`
+  );
+  image.setAttribute("width", `100%`);
+  div.append(image);
+  container.append(div);
+}
+
+function createElement() {}
+
+function randomNumberGenerator() {
+  return Math.ceil(100 + Math.random() * 899);
+}
+
+createAndAppendImages();
